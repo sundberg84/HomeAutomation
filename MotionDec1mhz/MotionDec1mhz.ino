a4add6e2a86458b08deaac24441e948d935f3f86
Andreas Sundberg <sundberg84@hotmail.com>
2017-05-31 19:53:54 +0200

    Update MotionDec1mhz.ino

--- MotionDec1mhz/MotionDec1mhz.ino
+++ MotionDec1mhz/MotionDec1mhz.ino
@@ -1,5 +1,5 @@
 // Enable debug prints
-//#define MY_DEBUG
+// #define MY_DEBUG
 
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
@@ -8,86 +8,72 @@
 // Set baud rate to same as optibot
 #define MY_BAUD_RATE 9600
 
-#define MY_NODE_ID 18
-#define SKETCH_NAME "Ringklocka Hall #18"
+#define MY_NODE_ID 16
+#define SKETCH_NAME "Dec Köket #16"
 #define SKETCH_VERSION "1.0"
 
 #include <MySensors.h>
 #include <Vcc.h>
 #include <SPI.h>
 
-unsigned long SLEEP_TIME = 86400000; // Sleep time between reports (in milliseconds)
-#define PIR_PIN 3
-#define BATTERY_REPORT_DAY 2   // Desired heartbeat(battery report) interval when inactive. 
-#define BATTERY_REPORT_BY_IRT_CYCLE 10  // Make a battery report after this many trips. Maximum report interval will also be equal to this number of days.
+#define CHILD_ID 1   // Id of the sensor child
+#define PIR_PIN 3   // The digital input you attached your motion sensor.  (Only 2 and 3 generates interrupt!)
+
+int BATTERY_REPORT_BY_IRT_CYCLE = 10;  // Make a battery report after this many trips. Maximum report interval will also be equal to this number of days.
 #define ONE_DAY_SLEEP_TIME 86400000
+bool interruptReturn = false; // "false" will make the first loop disregard high output from HV-505 (from start-up) and make a battery report instead.  
 #define VCC_MIN 1.9
 #define VCC_MAX 3.3
-#define CHILD_ID 0
-int dayCounter = BATTERY_REPORT_DAY;
-int irtCounter = 0;
-bool interruptReturn = false; // "false" will make the first loop disregard high output from HV-505 (from start-up) and make a battery report instead.
-int tripped = 0;
 
-
-Vcc vcc;
+// Initialize motion message
 MyMessage msg(CHILD_ID, V_TRIPPED);
 
-void setup()
-{
-#ifdef MY_DEBUG
-  Serial.println("Serial started");
-#endif
-  delay(100); // to settle power for radio
-  pinMode(PIR_PIN, INPUT);
+Vcc vcc;
+
+void setup()  
+{  
+  pinMode(PIR_PIN, INPUT);      // sets the motion sensor digital pin as input
 }
 
-void presentation()
-{
+void presentation()  {
   // Send the sketch version information to the gateway and Controller
   sendSketchInfo(SKETCH_NAME, SKETCH_VERSION);
 
   // Register all sensors to gw (they will be created as child devices)
-  present(CHILD_ID, S_MOTION);
-}
+  present(CHILD_ID, S_MOTION);}
 
-void loop()
+void loop()     
 {
 
-#ifdef MY_DEBUG
-  Serial.print("Wakeupcall: "); Serial.println(interruptReturn);
-    Serial.print("digitalRead(PIR_PIN): "); Serial.println(digitalRead(PIR_PIN));
-#endif
+      #ifdef MY_DEBUG
+       Serial.print("Wakeupcall: "); Serial.println(interruptReturn);
+      #endif
 
-  if (interruptReturn == true) {    // Woke up by changing pin
-
+    if (interruptReturn == true) {    // Woke up by changing pin
     send(msg.set("1"));
-
-    irtCounter++;
-    if (irtCounter >= BATTERY_REPORT_BY_IRT_CYCLE) {
-      irtCounter = 0;
-      sendBatteryReport();
+    BATTERY_REPORT_BY_IRT_CYCLE++;
+        
+          if (BATTERY_REPORT_BY_IRT_CYCLE>9){
+          BATTERY_REPORT_BY_IRT_CYCLE=0;
+          sendBatteryReport();
+          }
     }
 
-  }
-
-  if (interruptReturn == false) { // Woke up by timer  (or it's the first run)
-    dayCounter++;
-    if (dayCounter >= BATTERY_REPORT_DAY) {
-      dayCounter = 0;
+  if (interruptReturn == false) {
       sendBatteryReport();
     }
-  }
 
 #ifdef MY_DEBUG
   Serial.println("5 sec");
 #endif
-  sleep(3000);  // Make sure everything is stable before start to sleep with interrupts. (don't use "wait()" here). Tests shows false trip ~2s after battery report otherwise.
+ 
+  sleep(10000);  // Make sure everything is stable before start to sleep with interrupts. (don't use "wait()" here). Tests shows false trip ~2s after battery report otherwise.
   send(msg.set("0"));
-  sleep(5000);  // Make sure everything is stable before start to sleep with interrupts. (don't use "wait()" here). Tests shows false trip ~2s after battery report otherwise.
+  sleep(500);  // Make sure everything is stable before start to sleep with interrupts. (don't use "wait()" here). Tests shows false trip ~2s after battery report otherwise.
 
  // Sleep until interrupt comes in on motion sensor or sleep time passed.
-  interruptReturn = sleep(digitalPinToInterrupt(PIR_PIN), RISING, SLEEP_TIME);
+  interruptReturn = sleep(digitalPinToInterrupt(PIR_PIN), RISING, ONE_DAY_SLEEP_TIME );
+  
 }
 
 void sendBatteryReport() {
@@ -98,4 +84,3 @@ void sendBatteryReport() {
 #endif
   sendBatteryLevel(batteryPcnt);
 }
-
